cf7cb2a3243b04f4b76619e7adae9e92f7460cd2
pedrokaroth <pedrokaroth@gmail.com>
2022-01-18 20:23:33 -0300

    Obtendo um paciente

--- handler.js
+++ handler.js
@@ -19,7 +19,7 @@ module.exports.listarPacientes = async (event) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data.Items)
     }
   } catch (err) {
     console.log("Error: ", err);
@@ -34,19 +34,40 @@ module.exports.listarPacientes = async (event) => {
 };
 
 module.exports.obterPaciente = async (event) => {
+  console.log(event.pathParameters);
+  try {
+    const pacienteId = event.pathParameters;
+    console.log(pacienteId);
 
-  const { id } = event.pathParameters;
-  const paciente = pacientes.find((paciente) => paciente.id == id);
+    const data = await dynameDb.get({
+      ...params,
+      Key: {
+        "paciente_id": pacienteId.id
+      },
+    }).promise();
 
-  if (!paciente) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({error: "Paciente não encontrado"})
+    if (!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({error: "Paciente não existe"}, null, 2)
+      }
     }
-  }
+
+    const paciente = data.Item;
 
     return {
       statusCode: 200,
-      body: JSON.stringify(paciente, null, 2),
-    };
+      body: JSON.stringify(data.Item)
+    }
+
+  } catch (err) {
+    console.log("Error: ", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error"
+      })
+    }
+  }
 };
